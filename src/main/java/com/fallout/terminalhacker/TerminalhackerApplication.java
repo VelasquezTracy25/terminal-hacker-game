84f61e819617b3f7f3cddb5cb373dcb924dcb3fb
Tracy Velasquez <velasqueztracy25@gmail.com>
2021-03-14 17:45:12 -0500

    first commit

---
+++ src/main/java/com/fallout/terminalhacker/TerminalhackerApplication.java
@@ -0,0 +1,13 @@
+package com.fallout.terminalhacker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalhackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerminalhackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fallout/terminalhacker/TerminalhackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fallout.terminalhacker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalhackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
